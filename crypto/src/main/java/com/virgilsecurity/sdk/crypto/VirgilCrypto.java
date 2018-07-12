faee0d88ef434e8c2fe120b7e86bae81a0dcbfb7
Andrii Iakovenko <iakovenko.andrii@gmail.com>
2018-07-12 10:41:13 +0300

    Fix Virgil Crypto for Android

--- crypto/src/main/java/com/virgilsecurity/sdk/crypto/VirgilCrypto.java
+++ crypto/src/main/java/com/virgilsecurity/sdk/crypto/VirgilCrypto.java
@@ -73,9 +73,9 @@ import java.util.List;
 public class VirgilCrypto {
 
   private static final Charset UTF8_CHARSET = StandardCharsets.UTF_8;
-  private static final byte[] CUSTOM_PARAM_SIGNATURE = "VIRGIL-DATA-SIGNATURE"
+  public static final byte[] CUSTOM_PARAM_SIGNATURE = "VIRGIL-DATA-SIGNATURE"
       .getBytes(UTF8_CHARSET);
-  private static final byte[] CUSTOM_PARAM_SIGNER_ID = "VIRGIL-DATA-SIGNER-ID"
+  public static final byte[] CUSTOM_PARAM_SIGNER_ID = "VIRGIL-DATA-SIGNER-ID"
       .getBytes(UTF8_CHARSET);
 
   private KeysType defaultKeyPairType;

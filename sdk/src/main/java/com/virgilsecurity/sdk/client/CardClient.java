3e30d9e209800c87059abf75915cc56ae3c5dcb9
Andrii Iakovenko <iakovenko.andrii@gmail.com>
2018-12-03 20:16:18 +0200

    Fix CardClient javadocs

--- sdk/src/main/java/com/virgilsecurity/sdk/client/CardClient.java
+++ sdk/src/main/java/com/virgilsecurity/sdk/client/CardClient.java
@@ -1,11 +1,44 @@
-package com.virgilsecurity.sdk.client;
+/*
+ * Copyright (c) 2015-2018, Virgil Security, Inc.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *     (3) Neither the name of virgil nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-import java.util.Collection;
-import java.util.List;
+package com.virgilsecurity.sdk.client;
 
 import com.virgilsecurity.sdk.cards.model.RawSignedModel;
 import com.virgilsecurity.sdk.client.exceptions.VirgilServiceException;
 import com.virgilsecurity.sdk.utils.Tuple;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Interface representing operations with Virgil Cards service.
@@ -18,10 +51,13 @@ public interface CardClient {
   /**
    * Get card from Virgil Services by specified identifier.
    *
-   * @param cardId the card identifier.
-   * @param token  token to authorize the request.
+   * @param cardId
+   *          the card identifier.
+   * @param token
+   *          token to authorize the request.
    * @return the card loaded from Virgil Cards service.
-   * @throws VirgilServiceException if service call failed
+   * @throws VirgilServiceException
+   *           if service call failed
    */
   public Tuple<RawSignedModel, Boolean> getCard(String cardId, String token)
       throws VirgilServiceException;
@@ -29,10 +65,13 @@ public interface CardClient {
   /**
    * Publishes card in Virgil Cards service.
    *
-   * @param rawCard raw signed model of card to be published.
-   * @param token   token to authorize the request.
+   * @param rawCard
+   *          raw signed model of card to be published.
+   * @param token
+   *          token to authorize the request.
    * @return the {@link RawSignedModel} of the Card that is published to Virgil Cards service.
-   * @throws VirgilServiceException if an error occurred while publishing Card.
+   * @throws VirgilServiceException
+   *           if an error occurred while publishing Card.
    */
   public RawSignedModel publishCard(RawSignedModel rawCard, String token)
       throws VirgilServiceException;
@@ -40,10 +79,13 @@ public interface CardClient {
   /**
    * Search cards Virgil Services by specified identity.
    *
-   * @param identity the identity for search.
-   * @param token    token to authorize the request.
+   * @param identity
+   *          the identity for search.
+   * @param token
+   *          token to authorize the request.
    * @return A list of found cards.
-   * @throws VirgilServiceException if service call failed
+   * @throws VirgilServiceException
+   *           if service call failed
    */
   public List<RawSignedModel> searchCards(String identity, String token)
       throws VirgilServiceException;
@@ -51,10 +93,13 @@ public interface CardClient {
   /**
    * Search cards Virgil Services by specified identity.
    *
-   * @param identities the identity for search.
-   * @param token      token to authorize the request.
+   * @param identities
+   *          the identity for search.
+   * @param token
+   *          token to authorize the request.
    * @return A list of found cards.
-   * @throws VirgilServiceException if service call failed
+   * @throws VirgilServiceException
+   *           if service call failed
    */
   public List<RawSignedModel> searchCards(Collection<String> identities, String token)
       throws VirgilServiceException;
